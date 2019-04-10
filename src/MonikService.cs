6644b33c1aeb4636581be8cf9ccca4bc14852adb
Михаил Михайлов <m.mikhailov@smartdriving.io>
2019-04-10 13:22:42 +0300

    Change timeouts - separate LoadTimeout and RequestTimeout

--- src/MonikService.cs
+++ src/MonikService.cs
@@ -1,18 +1,19 @@
-﻿using System.IO;
-using System.Net;
-using MonikTerminal.Interfaces;
+﻿using MonikTerminal.Interfaces;
 using MonikTerminal.ModelsApi;
 using Newtonsoft.Json;
-using System.Threading.Tasks;
+using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MonikTerminal
 {
-	public class MonikService : IMonikService
-	{
+    public class MonikService : IMonikService
+    {
+        private static readonly TimeSpan LoadTimeout = TimeSpan.FromMinutes(1);
+        private static readonly TimeSpan RequestTimeout = TimeSpan.FromSeconds(5);
+
 		private readonly IConfig _cfg;
 
 		public MonikService(IConfig aCfg)
@@ -22,21 +23,21 @@ namespace MonikTerminal
 
 		public async Task<EGroup[]> GetGroups()
 		{
-			var json = await GetJson("groups");
+			var json = await GetJson("groups", LoadTimeout);
 			var result = JsonConvert.DeserializeObject<EGroup[]>(json);
 			return result;
 		}
 
 		public async Task<EInstance[]> GetInstances()
 		{
-			var json = await GetJson("instances");
+			var json = await GetJson("instances", LoadTimeout);
 			var result = JsonConvert.DeserializeObject<EInstance[]>(json);
 			return result;
 		}
 
 		public async Task<ESource[]> GetSources()
 		{
-			var json = await GetJson("sources");
+			var json = await GetJson("sources", LoadTimeout);
 			var result = JsonConvert.DeserializeObject<ESource[]>(json);
 			return result;
 		}
@@ -44,7 +45,7 @@ namespace MonikTerminal
 		public async Task<ELog_[]> GetLogs(ELogRequest aRequest)
 		{
 			var reqJson = JsonConvert.SerializeObject(aRequest);
-			var resJson = await PostJson("logs5", reqJson);
+			var resJson = await PostJson("logs5", reqJson, RequestTimeout);
 
 			var result = JsonConvert.DeserializeObject<ELog_[]>(resJson);
 			return result;
@@ -53,7 +54,7 @@ namespace MonikTerminal
 		public async Task<EKeepAlive_[]> GetKeepAlives(EKeepAliveRequest aRequest)
 		{
 			var reqJson = JsonConvert.SerializeObject(aRequest);
-			var resJson = await PostJson("keepalive2", reqJson);
+			var resJson = await PostJson("keepalive2", reqJson, RequestTimeout);
 
 			var result = JsonConvert.DeserializeObject<EKeepAlive_[]>(resJson);
 			return result;
@@ -61,24 +62,23 @@ namespace MonikTerminal
 
 	    public async Task<EMetricWindow[]> GetMetricsWindow()
 	    {
-	        var json = await GetJson("metrics/windows");
+	        var json = await GetJson("metrics/windows", RequestTimeout);
 	        var result = JsonConvert.DeserializeObject<EMetricWindow[]>(json);
 	        return result;
         }
 
 	    public async Task<EMetric[]> GetMetrics()
 	    {
-	        var json = await GetJson("metrics");
+	        var json = await GetJson("metrics", RequestTimeout);
 	        var result = JsonConvert.DeserializeObject<EMetric[]>(json);
 	        return result;
 	    }
 
 
-        private async Task<string> GetJson(string aMethod)
+        private async Task<string> GetJson(string aMethod, TimeSpan timeout)
 		{
-			var client = new HttpClient();
-			client.Timeout = TimeSpan.FromSeconds(5);
-			client.DefaultRequestHeaders.Accept.Clear();
+            var client = new HttpClient {Timeout = timeout};
+            client.DefaultRequestHeaders.Accept.Clear();
 			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 			client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
@@ -87,11 +87,10 @@ namespace MonikTerminal
 			return await stringTask;
 		}
 
-		private async Task<string> PostJson(string aMethod, string aJson)
+		private async Task<string> PostJson(string aMethod, string aJson, TimeSpan timeout)
 		{
-			var client = new HttpClient();
-			client.Timeout = TimeSpan.FromSeconds(5);
-			client.DefaultRequestHeaders.Accept.Clear();
+            var client = new HttpClient {Timeout = timeout};
+            client.DefaultRequestHeaders.Accept.Clear();
 			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 			client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
